1f870ed6a2201939b124e0481c44f782487b701f
thaleshXor <thalesh.sharma@sap.com>
2020-07-28 22:11:08 +0530

    TestBase import statement added

--- src/test/java/com/temp_test/tests/TempBase.java
+++ src/test/java/com/temp_test/tests/TempBase.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
